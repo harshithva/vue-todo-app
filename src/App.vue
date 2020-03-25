3ddf936114273efa8dad62fc8e6c1f0780da7185
harshithva <harshith11032001@gmail.com>
2020-03-25 20:17:12 +0530

    Router working

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,40 @@
 <template>
   <div id="app">
-    <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <div id="nav">
+      <Header />
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
-import AddTodo from "./components/AddTodo";
 import Header from "./components/layout/Header";
-import axios from "axios";
-
 export default {
   name: "app",
   components: {
-    Todos,
-    Header,
-    AddTodo
-  },
-  methods: {
-    deleteTodo(id) {
-      axios
-        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        .then((this.todos = this.todos.filter(todo => todo.id != id)))
-        .catch(err => console.log(err));
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-      axios
-        .post("https://jsonplaceholder.typicode.com/todos", {
-          title: title,
-          completed: completed
-        })
-        .then(res => (this.todos = [...this.todos, res.data]))
-        .catch(err => console.log(err));
-    }
-  },
-  data() {
-    return {
-      todos: []
-    };
-  },
-  created() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
-      .then(res => (this.todos = res.data))
-      .catch(err => console.log(err));
+    Header
   }
 };
 </script>
-
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
+#nav {
+  padding: 30px;
 }
 
-.btn {
-  display: inline-block;
-  border: none;
-  background: #555;
-  color: #fff;
-  padding: 7px 20px;
-  cursor: pointer;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-.btn:hover {
-  background: #666;
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <header class="header">
     <h1>TodoList</h1>
+    <div class="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
   </header>
 </template>
 
@@ -21,5 +25,6 @@ export default {
 .header a {
   color: #fff;
   padding-right: 5px;
+  text-decoration: none;
 }
 </style>
